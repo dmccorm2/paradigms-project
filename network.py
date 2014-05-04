c54806aa82fe89da734bca8c03ed7ca04363db94
dmccorm2 <dmccorm2@nd.edu>
2014-05-03 21:45:09 -0400

    Declared globals in network.py for kw
    kw is dictionary of keywords to pass as args to the single iteration
    function

---
+++ network.py
@@ -0,0 +1,6 @@
+DIR = 'direction' # 'left', 'right', 'up', 'down'?
+COORD = 'xy' #'x, y tuple'
+ANGLE = 'angle' # 'degrees from 0'
+FIRE = 'fired' # 'bool if fired projectile or not'
+HEALTH = 'health' # 'current health to check death'
+
